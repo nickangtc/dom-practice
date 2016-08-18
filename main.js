0bbd6c021558b484410a55f3439748f3aecfbebf
nickangtc <nickangtc@gmail.com>
2016-08-18 17:23:39 +0800

    added button clicks

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log("hello world!");
+
+document.addEventListener('DOMContentLoaded', function() {
+  function setInputAsTitle () {
+    var title = document.getElementById("title");
+    var input = document.getElementsByTagName("input")[0];
+    title.textContent = input.value;
+  }
+
+  document.getElementById("set-title").addEventListener("click", function() {
+    console.log("set title button clicked");
+    setInputAsTitle();
+  });
+});
